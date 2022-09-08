8a5fdfd0b4acfaa6f2ab64ee4eb9752945fc337a
Peter luque <Peterluque21@gmail.com>
2022-09-08 18:37:19 -0400

    initial commit

---
+++ string- interpolation- syntax sugars/Program.cs
@@ -0,0 +1,24 @@
+﻿            }
+
+            //int answer = 4; //exclicit typing
+            //string response; //explicit typing
+
+            //if (answer < 9)
+            //{
+           //     response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //   response = answer + " greater than or equal to nine";
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal too nine";
+
+            Console.WriteLine(response);
+
+
+
+    }          
+
+      
